68972f8e567d048cc0c5d55d29cfca8ce8a47825
zhoujingxin <zhoujingxin@360.cn>
2018-11-26 09:58:45 +0800

    补充HashMap示例

--- CollectionsSample/src/com/zjx/example/HashMapTest.java
+++ CollectionsSample/src/com/zjx/example/HashMapTest.java
@@ -6,10 +6,20 @@ import java.util.Map;
 public class HashMapTest {
 
     public void testHashMap() {
-        Map<String, String> map = new HashMap<>();
+        HashMap<String, String> map = new HashMap<>();
         map.put(null, null); // key和value都可以为null
         map.put(null, "hello world");
-        System.out.println(map.get(null));
+        System.out.println("null -> " + map.get(null));
+
+        map.put("A", "Hello");
+        map.put("B", "World");
+        map.put("C", "By");
+        map.put("D", "Java");
+        map.put("D", "Kotlin");
+        System.out.println("------------------------for each------------------------");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
     }
 
 }
